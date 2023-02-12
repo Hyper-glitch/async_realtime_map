439a01bffe2b31c08fe7817ef7fea2b1d9abdae9
hyper-glitch <graff049@gmail.com>
2023-02-12 16:23:07 +0300

    feat(client): add many bus routes

--- fake_bus.py
+++ fake_bus.py
@@ -1,23 +1,26 @@
-import json
 from sys import stderr
 
 import trio
 from trio_websocket import open_websocket_url
 
 from dto import RouteDTO, BusDTO
+from route_helper import load_routes
+
+
+async def run_bus(route):
+    async with open_websocket_url("ws://127.0.0.1:8080") as ws:
+        route_dto = RouteDTO(**route)
+        for lat, lng in route_dto.coordinates:
+            bus_dto = BusDTO(busId=route_dto.name, lat=lat, lng=lng, route=route_dto.name)
+            await ws.send_message(bus_dto.json())
+            await trio.sleep(3)
 
 
 async def fake_bus_client():
     try:
-        async with open_websocket_url("ws://127.0.0.1:8000") as ws:
-            with open('routes/156.json', mode='r') as file:
-                content = file.read()
-            route = json.loads(content)
-            route_dto = RouteDTO(**route)
-            for lat, lng in route_dto.coordinates:
-                bus_dto = BusDTO(busId="c790с", lat=lat, lng=lng, route="156")
-                await ws.send_message(bus_dto.json())
-                await trio.sleep(3)
+        async with trio.open_nursery() as nursery:
+            for route in load_routes():
+                nursery.start_soon(run_bus, route)
     except OSError as ose:
         print('Connection attempt failed: %s' % ose, file=stderr)
 
