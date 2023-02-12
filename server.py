f2f3a3549567bdbb362f087bd288c01eab2b3130
hyper-glitch <graff049@gmail.com>
2023-02-12 16:28:00 +0300

    feat(server): add server for sending buses to browser

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
+import trio
 from pydantic.error_wrappers import ValidationError
 from trio_websocket import ConnectionClosed
 
-from dto import ClientMessageDTO, BusDTO
+from dto import ClientMessageDTO, BusDTO, ServerMessageDTO
+
+BUSES: list[BusDTO] = []
 
 
 async def server(request):
@@ -11,9 +14,22 @@ async def server(request):
         message = await ws.get_message()
         try:
             dto = BusDTO.parse_raw(message)
+            BUSES.append(dto)
         except ValidationError:
             dto = ClientMessageDTO.parse_raw(message)
-        except ConnectionClosed:
+        except (ConnectionClosed, BaseException):
             break
         finally:
             print(dto)
+
+
+async def send_to_browser(request):
+    ws = await request.accept()
+    while True:
+        server_msg_dto = ServerMessageDTO(msgType="Buses", buses=BUSES)
+        try:
+            await ws.send_message(server_msg_dto.json())
+            BUSES.clear()
+            await trio.sleep(1)
+        except (ConnectionClosed, BaseException):
+            break
