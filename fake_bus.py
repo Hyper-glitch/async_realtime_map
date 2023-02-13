ca0fc0d89b9dd442244a6bc88c881e90894dcbfb
hyper-glitch <graff049@gmail.com>
2023-02-13 18:43:16 +0300

    feat(client): add two paths for buses

--- fake_bus.py
+++ fake_bus.py
@@ -7,11 +7,15 @@ from dto import RouteDTO, BusDTO
 from route_helper import load_routes
 
 
-async def run_bus(route):
+def generate_bus_id(route_id, bus_index):
+    return f"{route_id}-{bus_index}"
+
+
+async def run_bus(route, route_dto, idx):
+    bus_id = generate_bus_id(route_id=route_dto.name, bus_index=generate_bus_id(route_id=route_dto.name, bus_index=idx))
     async with open_websocket_url("ws://127.0.0.1:8080") as ws:
-        route_dto = RouteDTO(**route)
-        for lat, lng in route_dto.coordinates:
-            bus_dto = BusDTO(busId=route_dto.name, lat=lat, lng=lng, route=route_dto.name)
+        for lat, lng in route:
+            bus_dto = BusDTO(busId=bus_id, lat=lat, lng=lng, route=route_dto.name)
             await ws.send_message(bus_dto.json())
             await trio.sleep(3)
 
@@ -20,7 +24,14 @@ async def fake_bus_client():
     try:
         async with trio.open_nursery() as nursery:
             for route in load_routes():
-                nursery.start_soon(run_bus, route)
+                route_dto = RouteDTO(**route)
+                middle_route_index = len(route_dto.coordinates) / 2
+                start_middle_path = route_dto.coordinates[:round(middle_route_index)]
+                end_middle_path = route_dto.coordinates[round(middle_route_index):]
+                paths = [start_middle_path, end_middle_path]
+                for idx, path in enumerate(paths):
+                    nursery.start_soon(run_bus, path, route_dto, idx)
+
     except OSError as ose:
         print('Connection attempt failed: %s' % ose, file=stderr)
 
